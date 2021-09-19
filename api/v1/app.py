60ccfde3d4591bfa662fdc2e779faaeef9dda0f5
Matteo-lu <mateolondono.u@gmail.com>
2021-09-19 13:11:40 -0500

    Updating with error 404 not foung

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,8 @@ endpoint (route) that will be to return the API status
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
+from flask import make_response
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,6 +19,12 @@ app.strict_slashes = False
 def show_teardown(exception):
     """method to handle teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """handler for 404 errors that returns a JSON-formatted 404 response"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
