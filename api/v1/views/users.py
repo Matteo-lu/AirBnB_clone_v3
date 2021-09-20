db13628a25e7f948fec802a634cd61654082d4c5
Matteo-lu <mateolondono.u@gmail.com>
2021-09-20 17:24:16 -0500

    user routes creation

---
+++ api/v1/views/users.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""
+module to create a new view for State objects that
+handles all default RESTFul API actions
+
+"""
+
+from api.v1.views import app_views
+from flask import Flask
+from flask import jsonify
+from models.state import State
+from models.user import User
+from models import storage
+from flask import request
+from flask import abort
+
+
+@app_views.route('/users', strict_slashes=False)
+def retrive_users():
+    """Retrieves the list of all State objects"""
+    obj_list = []
+    obj_dict = storage.all(User)
+    for value in obj_dict.values():
+        obj_list.append(value.to_dict())
+    return (jsonify(obj_list))
+
+
+@app_views.route('/users/<user_id>', strict_slashes=False)
+def retrive_user_by_id(user_id):
+    """Retrieves a State object"""
+    try:
+        obj = storage.get(User, user_id)
+        return (jsonify(obj.to_dict()))
+    except:
+        abort(404)
+
+
+@app_views.route(
+    '/users/<user_id>',
+    strict_slashes=False,
+    methods=['DELETE']
+)
+def delete_user_by_id(user_id):
+    """Deletes a State object"""
+    try:
+        obj = storage.get(User, user_id)
+        State.delete(obj)
+        storage.save()
+        return ({}), 200
+    except:
+        abort(404)
+
+
+@app_views.route('/users', strict_slashes=False,  methods=['POST'])
+def create_user():
+    """Creates a State"""
+    json_data = request.get_json()
+    if not (json_data):
+        abort(400, 'Not a JSON')
+    elif ('name' not in json_data.keys()):
+        abort(400, 'Missing name')
+    else:
+        new_user = User()
+        new_user.name = json_data['name']
+        storage.new(new_user)
+        storage.save()
+        return (jsonify(State.to_dict(new_user)), 201)
+
+
+@app_views.route(
+                '/users/<user_id>',
+                strict_slashes=False,
+                methods=['PUT']
+                )
+def update_user(user_id):
+    """Updates a State object"""
+    try:
+        json_data = request.get_json()
+        if not (json_data):
+            abort(400, 'Not a JSON')
+        elif ('name' not in json_data.keys()):
+            abort(400, 'Missing name')
+        else:
+            obj = storage.get(User, user_id)
+            setattr(obj, 'name', json_data['name'])
+            storage.save()
+            return (jsonify(obj.to_dict()), 200)
+    except:
+        abort(404)
