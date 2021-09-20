318f4797197e74f0dfb6bb3214bafd83204a1074
Matteo-lu <mateolondono.u@gmail.com>
2021-09-20 12:27:12 -0500

    Validation correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,16 @@ from flask import Flask
 from models import storage
 from flask import make_response
 from flask import jsonify
+from os import getenv
+
+if getenv("HBNB_API_HOST") is None:
+    HBNB_API_HOST = '0.0.0.0'
+else:
+    HBNB_API_HOST = getenv("HBNB_API_HOST")
+if getenv("HBNB_API_PORT") is None:
+    HBNB_API_PORT = '5000'
+else:
+    HBNB_API_PORT = getenv("HBNB_API_PORT")
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -28,4 +38,4 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000", threaded=True)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
